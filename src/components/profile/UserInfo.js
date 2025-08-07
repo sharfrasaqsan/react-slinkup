aad97c7960d1753ec1e3b2adde3eb1b575846a89
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2025-08-08 01:38:35 +0530

    follow ongoing

--- src/components/profile/UserInfo.js
+++ src/components/profile/UserInfo.js
@@ -1,13 +1,39 @@
+import { toast } from "react-toastify";
 import { useAuth } from "../../contexts/AuthContext";
+import { FaPlus } from "react-icons/fa";
+import { useState } from "react";
+import ButtonSpinner from "../../utils/ButtonSpinner";
 
 const UserInfo = () => {
   const { user } = useAuth();
 
+  const [followLoading, setFollowLoading] = useState(false);
+
   if (!user) return null;
+
+  const handleFollow = async (userId) => {
+    setFollowLoading(true);
+    try {
+    } catch (err) {
+      toast.error(err.message);
+    }
+    setFollowLoading(false);
+  };
 
   return (
     <div>
       <p>@{user.username}</p>
+      <button ooclick={() => handleFollow(user.id)}>
+        {followLoading ? (
+          <>
+            Following... <ButtonSpinner />
+          </>
+        ) : (
+          <>
+            Follow <FaPlus />
+          </>
+        )}
+      </button>
       <p>
         <span>
           {user.firstname} {user.lastname}
