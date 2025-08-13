803da11744e08823f73303d73ef6f2eb1f9c417c
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2025-08-13 20:35:03 +0530

    create post ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,15 @@ import CreatePost from "../components/post/CreatePost";
 
 const Home = () => {
   return (
-    <section>
-      <CreatePost />
-      <Feed />
+    <section className="container py-4">
+      <div className="row">
+        <div className="col-12 col-lg-4 mb-4">
+          <CreatePost />
+        </div>
+        <div className="col-12 col-lg-8">
+          <Feed />
+        </div>
+      </div>
     </section>
   );
 };
