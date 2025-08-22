121ddd81eb2f72b21ccafc6dd2537e66385d0f37
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2025-08-22 19:51:34 +0530

    suggest user & explore enhanced

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -5,30 +5,36 @@ import NotFound from "../utils/NotFound";
 import _ from "lodash";
 import "../styles/Explore.css";
 import { Link } from "react-router";
+import PostCard from "../components/feed/PostCard";
+import { useMemo } from "react";
 
 const Explore = () => {
   const { user } = useAuth();
   const { posts, loading } = useData();
 
+  // useMemo - Caches a value from a calculation (To avoid recalculating on every render)
+  // useEffect - Runs a function after render for side effects (To do things outside React (API, DOM, subscriptions))
+
+  const expplorePosts = posts?.filter(
+    (post) => post.userId !== user.id && !user.following.includes(post.userId)
+  );
+
+  const randomPosts = useMemo(() => {
+    return _.sampleSize(expplorePosts, 30);
+  }, [expplorePosts]);
+
   if (loading) return <LoadingSpinner />;
   if (!user) return <NotFound text={"No user found! Please log in."} />;
   if (posts.length === 0) return <NotFound text={"No posts found!"} />;
-
-  const randomPosts = _.sampleSize(posts, 20);
+  if (randomPosts.length === 0) return <NotFound text={"No posts found!"} />;
 
   return (
     <section className="container py-4">
-      <div className="row g-4">
-        {randomPosts.map((post) => (
-          <div className="col-md-6 col-lg-4" key={post.id}>
+      <div className="masonry">
+        {randomPosts?.map((post) => (
+          <div className="masonry-item" key={post.id}>
             <Link to={`/post/${post.id}`}>
-              <div className="explore-card h-100">
-                <div className="explore-card-body">
-                  {post.body.length > 100
-                    ? `${post.body.slice(0, 100)}...`
-                    : post.body}
-                </div>
-              </div>
+              <PostCard post={post} />
             </Link>
           </div>
         ))}
