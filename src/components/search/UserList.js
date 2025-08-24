c649a0fe9f999045a8275db19157a757c1e78453
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2025-08-24 22:27:07 +0530

    following button added on the users list

--- src/components/search/UserList.js
+++ src/components/search/UserList.js
@@ -1,50 +1,68 @@
 import { Link } from "react-router";
 import UserAvatar from "../UserAvatar";
 import "../../styles/search/UserList.css";
+import FollowButton from "../users/FollowButton";
+import { useEffect, useState } from "react";
 
 const UserList = ({ searchedUser, user }) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    const following = searchedUser.followers?.includes(user.id);
+    setIsFollowing(following);
+  }, [searchedUser, user.id]);
+
   return (
     <>
       <li key={searchedUser.id} className="user-list-item">
-        {user.id === searchedUser.id ? (
-          <Link to={`/profile/${user.id}`}>
-            <UserAvatar
-              width="40px"
-              height="40px"
-              fontSize="40px"
-              user={user}
-            />
-            {searchedUser.username}
-          </Link>
-        ) : (
-          <Link to={`/user/${searchedUser.id}`}>
-            <div className="d-flex align-items-center">
+        <div className="d-flex align-items-center justify-content-between w-100">
+          {user.id === searchedUser.id ? (
+            <Link to={`/profile/${user.id}`}>
               <UserAvatar
                 width="40px"
                 height="40px"
                 fontSize="40px"
-                user={searchedUser}
+                user={user}
               />
-              <div>
-                {searchedUser?.username}
-                <br />
-                <span className="text-muted form-text">
-                  {!searchedUser?.followers ||
-                  searchedUser.followers.length === 0 ? (
-                    <span>New User</span>
-                  ) : (
-                    <>
-                      {searchedUser.followers.length}{" "}
-                      {searchedUser.followers.length === 1
-                        ? "follower"
-                        : "followers"}
-                    </>
-                  )}
-                </span>
+              {searchedUser.username}
+            </Link>
+          ) : (
+            <Link to={`/user/${searchedUser.id}`}>
+              <div className="d-flex align-items-center">
+                <UserAvatar
+                  width="40px"
+                  height="40px"
+                  fontSize="40px"
+                  user={searchedUser}
+                />
+                <div>
+                  {searchedUser?.username}
+                  <br />
+                  <span className="text-muted form-text">
+                    {!searchedUser?.followers ||
+                    searchedUser.followers.length === 0 ? (
+                      <span>New User</span>
+                    ) : (
+                      <>
+                        {searchedUser.followers.length}{" "}
+                        {searchedUser.followers.length === 1
+                          ? "follower"
+                          : "followers"}
+                      </>
+                    )}
+                  </span>
+                </div>
               </div>
-            </div>
-          </Link>
-        )}
+            </Link>
+          )}
+
+          <FollowButton
+            user={user}
+            existUser={searchedUser}
+            alreadyFollowed={isFollowing}
+            setAlreadyFollowed={setIsFollowing}
+          />
+        </div>
       </li>
     </>
   );
